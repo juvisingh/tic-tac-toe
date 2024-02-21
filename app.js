88b546c91d7d5937be1359600d4cc9906bbba8f8
juvisingh <141285456+juvisingh@users.noreply.github.com>
2024-02-20 18:17:41 -0700

    updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var count = 0
 function whenClicked(box) {
     if (document.getElementById('playerTurn').innerHTML == 'Player: X') {
         if (document.getElementById(box).innerHTML == 'X' || document.getElementById(box).innerHTML == 'O') {
-            break;
+            return;
         }
         else {
             document.getElementById(box).innerHTML = "X";
@@ -10,11 +10,24 @@ function whenClicked(box) {
             count++;
             document.getElementById('turn').innerHTML = 'Turn: ' + count;
         }
+        getWinner(box,"X")
     }
     else {
         document.getElementById(box).innerHTML = "O";
         document.getElementById('playerTurn').innerHTML = 'Player: X'
         count++;
         document.getElementById('turn').innerHTML = 'Turn: ' + count;
+        getWinner(box,"O")
+    }
+    
+    
+}
+function getWinner(box, letter) {
+    console.log(box)
+    if (document.getElementById('a1').innerHTML == "X" && document.getElementById('a2').innerHTML == "X" && document.getElementById('a3').innerHTML == "X") {    
+        document.getElementById(box).innerHTML= letter
+            
+        alert('X Won!')
     }
 }
+    
